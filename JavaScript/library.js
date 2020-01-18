67fb1c737a36538f9e9f66c7166de1962a587407
Niclas <nxclxs.rist@gmail.com>
2020-01-18 19:20:35 +0100

    library

--- JavaScript/library.js
+++ JavaScript/library.js
@@ -1,41 +1,131 @@
 const math = require('mathjs');
 
-class Formula {
-	constructor(name, children = [], equation = '', value = 0) {
+let treePath = [];
+
+class Quantity {
+	constructor(name, value, formula, parents) {
 		this.name = name;
-		this.children = children;
-		this.equation = equation;
 		this.value = value;
+		this.formula = formula;
+		this.parents = parents;
 	}
-
-	getChildren = () => {
-		return this.children.map((child) => child.name);
-	};
-
-	setValue = (val) => {
-		this.value = val;
-	};
-
-	getFormula = () => {
-		return this.equation;
-	};
-
-	evalFormula = () => {
-		return math.evaluate(this.equation);
-	};
 }
 
-let m = new Formula('masse', [], '', 1);
-let g = new Formula('gewichtskraft', [], '', 9.81);
-let s = new Formula('strecke', [], '', 20);
-let l = new Formula('länge', [], '', 2);
+let W_el, F_el, E_el, l, q, mgs, m, g, s;
 
-let F_el = new Formula(
-	'elektrische kraft',
-	[m, g, s, l],
-	`(${m.value} * ${g.value} * ${s.value}) / ${l.value}`
+m = new Quantity('Masse', null, [[]], [['mul', mgs]]);
+
+g = new Quantity('Fallbeschleunigung', null, [[]], [['mul', mgs]]);
+
+s = new Quantity('Strecke', null, [[]], [['mul', mgs]]);
+l = new Quantity('Länge', null, [[]], [[F_el]]);
+
+mgs = new Quantity('m * g * s', null, [['mul', m, g, s]], [['div1', F_el]]);
+
+E_el = new Quantity('Elektrische Feldstärke', null, [[]], [['mul', F_el]]);
+
+F_el = new Quantity(
+	'elektrishce Feldkraft',
+	null,
+	[
+		['div', mgs, l],
+		['mul', E_el, q]
+	],
+	[['mul', W_el]]
 );
 
-console.log(F_el.getChildren());
-console.log(F_el.getFormula());
-console.log(F_el.evalFormula());
+W_el = new Quantity('elektrische Energie', null, [['mul', F_el, s]], [[]]);
+
+function assignValues(given) {
+	//given.map(e, idx => { e[0].value = e[1]; });
+	for (var i in given) given[i][0].value = given[i][1];
+}
+
+function addToTreePath(node) {
+	treePath.push(node);
+}
+
+function calculate(searched) {
+	let prod = 1;
+	let sum = 0;
+	let quot;
+
+	if (searched.formula !== [[]]) {
+		for (var i in searched.formula) {
+			if (searched.formula[i][0] === 'mul') {
+				for (j = 1; j < searched.formula[i].length; j++) {
+					if (searched.formula[i][j].value !== null) {
+						prod *= searched.formula[i][j].value;
+					} else {
+						break;
+					}
+				}
+
+				return prod;
+			}
+
+			if (searched.formula[i][0] === 'add') {
+				for (j = 1; j < searched.formula[i].length; j++) {
+					if (searched.formula[i][j].value != null) {
+						sum += searched.formula[i][j].value;
+					} else {
+						break;
+					}
+				}
+
+				return sum;
+			}
+
+			if (searched.formula[i][0] == 'div') {
+				if (searched.formula[i][1] != null && searched.formula[i][2] != null) {
+					quot = searched.formula[i][1].value / searched.formula[i][2].value;
+				} else {
+					break;
+				}
+
+				return quot;
+			}
+		}
+	} else {
+		return false;
+	}
+}
+
+function goTreeUp(searched) {
+	if (searched.parents != [[]]) {
+		for (var i in searched.parents) {
+			if (searched.parents[i][1].value != null) {
+				treePath.push(searched.parents[i]);
+
+				if (searched.parents[i][0] == 'mul') {
+				}
+			} else {
+				goTreeUp();
+			}
+		}
+	} else {
+		return false;
+	}
+}
+
+function getSolution(given, searched) {
+	assignValues(given);
+	addToTreePath(searched);
+
+	let calc = calculate(searched);
+	if (calc == false) {
+		goTreeUp(searched);
+	} else {
+		return calc;
+	}
+}
+
+getSolution(
+	[
+		[s, 2],
+		[g, 3],
+		[F_el, 4],
+		[l, 5]
+	],
+	m
+);
